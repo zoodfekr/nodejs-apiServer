04a1f12a28f2f309b7e3a5c9c3d6c65669f3b70f
Ramin <100441714+zoodfekr@users.noreply.github.com>
2024-10-14 21:16:06 +0330

    second download

--- controller/download_controller.js
+++ controller/download_controller.js
@@ -5,10 +5,13 @@ export const downloader = (req, res) => {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
 
-  const filePath = path.join(__dirname,'..', 'files', 'file_1.csv')
+  let filename =
+    'A.Beautiful.Mind.2001.1080P.Bluray.Dubbed.Hardtsub.Zarfilm.mp4'
+
+  const filePath = path.join(__dirname, '..', 'files', filename)
   console.log('File path:', filePath) // چاپ مسیر فایل برای بررسی
 
-  res.download(filePath, 'file_1.csv', err => {
+  res.download(filePath, filename, err => {
     if (err) {
       console.error('Error downloading file:', err)
       res.status(500).send('Error downloading file')
